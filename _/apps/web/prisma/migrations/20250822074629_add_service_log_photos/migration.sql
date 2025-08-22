854093d280fe758888c1f516be811eb70e7a8ab5
Robert Rully Pernando <robert.rully@gmail.com>
2025-08-22 14:56:48 +0700

    feat(prisma): add service log photos migration

--- _/apps/web/prisma/migrations/20250822074629_add_service_log_photos/migration.sql
+++ _/apps/web/prisma/migrations/20250822074629_add_service_log_photos/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE "public"."materials" (
     "name" TEXT NOT NULL,
     "stock" INTEGER NOT NULL DEFAULT 0,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
     CONSTRAINT "materials_pkey" PRIMARY KEY ("id")
 );
 
@@ -12,7 +13,8 @@ CREATE TABLE "public"."service_log_materials" (
     "service_log_id" INTEGER NOT NULL,
     "material_id" INTEGER NOT NULL,
     "quantity" INTEGER NOT NULL,
-    CONSTRAINT "service_log_materials_pkey" PRIMARY KEY ("service_log_id", "material_id")
+
+    CONSTRAINT "service_log_materials_pkey" PRIMARY KEY ("service_log_id","material_id")
 );
 
 -- CreateTable
@@ -20,12 +22,15 @@ CREATE TABLE "public"."service_log_photos" (
     "id" SERIAL NOT NULL,
     "service_log_id" INTEGER NOT NULL,
     "url" TEXT NOT NULL,
+
     CONSTRAINT "service_log_photos_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
 ALTER TABLE "public"."service_log_materials" ADD CONSTRAINT "service_log_materials_service_log_id_fkey" FOREIGN KEY ("service_log_id") REFERENCES "public"."service_logs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
+-- AddForeignKey
 ALTER TABLE "public"."service_log_materials" ADD CONSTRAINT "service_log_materials_material_id_fkey" FOREIGN KEY ("material_id") REFERENCES "public"."materials"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
+-- AddForeignKey
 ALTER TABLE "public"."service_log_photos" ADD CONSTRAINT "service_log_photos_service_log_id_fkey" FOREIGN KEY ("service_log_id") REFERENCES "public"."service_logs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
